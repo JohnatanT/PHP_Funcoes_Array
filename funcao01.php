e4e1ad538449b3f0157659c95142e7ccf213e244
Francisco Johnatan Tavares Cavalcante <fcavalcante@localhost.localdomain>
2017-04-29 10:35:34 -0300

    print_r()  e  var_dump()

---
+++ funcao01.php
@@ -0,0 +1,9 @@
+<?php
+
+$n = array(5,6,3,7);
+
+//Função  print_r() para mostrar o vetor
+print_r($n);
+
+//Outra maneira de exibir é pelo var_dump(). Ele mostra o tipo do elemento e a quantidade de itens que tem no vetor.
+var_dump($n);
