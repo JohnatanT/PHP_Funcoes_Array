45a1ebe59c4f64a4f16e15f1ba6ff118623b51cd
Francisco Johnatan Tavares Cavalcante <fcavalcante@localhost.localdomain>
2017-04-29 10:58:55 -0300

    Gerenciamento dos primeiros  elementos

---
+++ funcao04.php
@@ -0,0 +1,13 @@
+<?php
+
+$v = array("b","k","j","c");
+print_r($v);
+
+//Adicionar um elemento no incio do vetor
+array_unshift($v,7);
+print_r($v);
+
+//Eliminar um elemento do incio do vetor
+array_shift($v);
+print_r($v);
+
